6a7be3ffb1ec501e437c0cad406f3145e259c093
ikmalif29 <ikmalfauzaeni@gmail.com>
2024-09-24 22:12:22 +0700

    update

--- src/component/Headers.jsx
+++ src/component/Headers.jsx
@@ -1,3 +1,6 @@
+import { FcAbout } from "react-icons/fc";
+import { SiHyperskill } from "react-icons/si";
+import { GiSkills } from "react-icons/gi";
 
 const Header = () => {
   return (
@@ -5,22 +8,33 @@ const Header = () => {
       <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
         <div className="flex h-16 items-center justify-between">
           <div className="flex-1 md:flex md:items-center md:gap-12">
-            <h1 className="font-bold text-2xl text-white hover:text-indigo-600 cursor-pointer">Imals Portofolio</h1>
+            <h1 className="font-bold text-2xl text-white hover:text-indigo-600 cursor-pointer">
+              Imals Portfolio
+            </h1>
           </div>
 
           <div className="md:flex md:items-center md:gap-12">
             <nav aria-label="Global" className="hidden md:block">
               <ul className="flex items-center gap-6 text-sm">
-                <li>
-                  <a className="text-white transition hover:text-indigo-600" href="#about"> About </a>
+                <li className="flex items-center gap-2">
+                  <FcAbout size={24} /> {/* Ukuran ikon diatur */}
+                  <a className="text-white transition hover:text-indigo-600" href="#about">
+                    About
+                  </a>
                 </li>
 
-                <li>
-                  <a className="text-white transition hover:text-indigo-600" href="#project"> Projects </a>
+                <li className="flex items-center gap-2">
+                  <SiHyperskill />
+                  <a className="text-white transition hover:text-indigo-600" href="#project">
+                    Projects
+                  </a>
                 </li>
 
-                <li>
-                  <a className="text-white transition hover:text-indigo-600" href="#skills"> Skills </a>
+                <li className="flex items-center gap-2">
+                  <GiSkills size={24} />
+                  <a className="text-white transition hover:text-indigo-600" href="#skills">
+                    Skills
+                  </a>
                 </li>
               </ul>
             </nav>
@@ -39,7 +53,7 @@ const Header = () => {
                 <button className="rounded bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    className="size-5"
+                    className="h-5 w-5"
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
